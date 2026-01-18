3812323d8aed27983c8a2922e3ccc35c3841d21c
Ing. Emil Chavez <emilchavez09@gmail.com>
2026-01-18 02:43:29 -0400

    Create 05_01_studentpractice.py

---
+++ 100_days_python_practice/05_01_studentpractice.py
@@ -0,0 +1,13 @@
+# You are given a list of exam scores
+# and you have to print out the highest score 
+# from the List. You will need to use what you have learnt about Lists,
+# For Loops and Conditionals to print out the highest score in the list of student_scores.
+
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+highest = 0
+
+for score in student_scores:
+    if score > highest:
+        highest = score
+    print(highest)
